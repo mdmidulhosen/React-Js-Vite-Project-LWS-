8b084eada4b73d3a1f18e6a37e86ae7d39542a0e
Kibria <muhammadmridulhosenkibria@gmail.com>
2023-06-12 16:01:21 +0600

    dynamic time setInterval function

--- src/component/ClickBtn.jsx
+++ src/component/ClickBtn.jsx
@@ -1,15 +1,27 @@
-import React, { useState } from 'react'
-function MyButton() {
+import React, { useState, useEffect } from 'react';
 
-const [count, setCount] = useState(0)
+function MyButton() {
+  const [count, setCount] = useState(0);
+  const [time, setTime] = useState(new Date().toLocaleTimeString());
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setTime(new Date().toLocaleTimeString());
+    }, 1000);
+
+    return () => {
+      clearInterval(interval);
+    };
+  }, []);
 
   return (
     <div>
-        <h1>{count}</h1>
-        <button className='btn btn-md btn-primary' onClick={()=>setCount(count - 1)}>Decreament</button>
-        <button className='btn btn-md btn-primary' onClick={()=>setCount(count + 1)}>Increament</button>
+      <h1>{count}</h1>
+      <button className='btn btn-md btn-primary' onClick={() => setCount(count - 1)}>Decrement</button>
+      <button className='btn btn-md btn-primary' onClick={() => setCount(count + 1)}>Increment</button>
+      <h1>Time is: {time}</h1>
     </div>
-  )
+  );
 }
 
-export default MyButton
+export default MyButton;
