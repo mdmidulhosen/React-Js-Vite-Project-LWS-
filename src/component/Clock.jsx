a9eea2f157becf227d71d2835eb6f7874f085253
Kibria <muhammadmridulhosenkibria@gmail.com>
2023-06-13 09:26:56 +0600

    Live time set using state, setState, componentDidMount and setInterval

--- src/component/Clock.jsx
+++ src/component/Clock.jsx
@@ -1,15 +1,27 @@
 import React, { Component } from "react";
 
 export class Clock extends Component {
+  constructor(props){
+    super(props)
+    this.state = {date: new Date()}
+  }
+
   render() {
     return (
       <div>
         <span className="text">
-          {new Date().toLocaleTimeString(this.props.locale)}
+          {this.state.date.toLocaleTimeString(this.props.locale)}
         </span>
       </div>
     );
   }
+
+  componentDidMount(){
+    setInterval(()=>{
+      this.setState({date: new Date()})
+    }, 1000)
+  }
+
 }
 
 export default Clock;
