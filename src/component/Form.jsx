b1d59d3d53c9c4315942631858e0b2a80e7a7a86
muhammadkibria <muhammadmridulhosenkibria>
2023-06-14 08:51:25 +0600

    multiple input fields handle in one onChange handler funtion and use multiple state and setState using if else

--- src/component/Form.jsx
+++ src/component/Form.jsx
@@ -2,24 +2,37 @@
 import React, { Component } from 'react'
 
 export class Form extends Component {
+
     state = {
-        title: "Mridul"
+        title: "Mridul",
+        text: "This is my text"
     }
 
-    handlerChange =(e)=>{
-        this.setState({
-            title:e.target.value
-        })
+    handlerChange = (e) => {
+        if(e.target.type === "text"){
+            this.setState({
+                title: e.target.value
+            })
+        }else if(e.target.type === "textarea"){
+            this.setState({
+                text: e.target.value
+            })
+        }else{
+            console.log("Nothing Is Here")
+        }
     }
 
-  render() {
-    const {title} = this.state;
-    return (
-      <div>
-        <input type="text" value={title} onChange={this.handlerChange}/>
-      </div>
-    )
-  }
+    render() {
+        const { title, text } = this.state;
+        return (
+            <div>
+                <input type="text" value={title} onChange={this.handlerChange} />
+                <br></br>
+                <br></br>
+                <textarea  name="textarea" value={text} onChange={this.handlerChange}></textarea>
+            </div>
+        )
+    }
 }
 
 export default Form
